bca222b0e3123720b496732e50e071b9caf0d03c
gurusamyganesh <ganeshmuthukumar.gurusamy@hitachivantara.com>
2023-05-30 15:17:44 -0500

    added stages

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,4 +1,10 @@
-def call() {
+def call(body) {
+
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
     echo "Hi there"
     def stage = new stages()
     stage.executeStages()
