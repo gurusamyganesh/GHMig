cc375b9ec754f2e3424846805f268f9abdcdabd1
gurusamyganesh <ganeshmuthukumar.gurusamy@hitachivantara.com>
2023-05-30 14:53:48 -0500

    added stages

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,3 +1,28 @@
 def call(String inputVal = 'test') {
     echo "Hi ${inputVal}."
+    executeStages();
+}
+
+def executeStages() {
+    checkout();
+    build();
+    test();
+    deploy();
+}
+
+def checkout() {
+    echo "checking out.."
+}
+
+
+def build() {
+    echo "building classess.."
+}
+
+def test() {
+    echo "testing services.."
+}
+
+def deploy() {
+    echo "deploying.."
 }
