bd5d9e5f25ac2271a97cd74ed3808da1545dd333
gurusamyganesh <ganeshmuthukumar.gurusamy@hitachivantara.com>
2023-05-30 15:14:35 -0500

    added stages

---
+++ vars/stages.groovy
@@ -0,0 +1,23 @@
+def executeStages() {
+    checkout();
+    build();
+    test();
+    deploy();
+}
+
+def checkout() {
+    echo "checking out.."
+}
+
+
+def build() {
+    echo "building classess.."
+}
+
+def test() {
+    echo "testing services.."
+}
+
+def deploy() {
+    echo "deploying.."
+}
--- vars/test.groovy
+++ vars/test.groovy
@@ -1,29 +1,6 @@
 def call() {
     echo "Hi there"
-    executeStages();
+    def stage = new stages()
+    stage.executeStages()
     echo "Hi stages"
-}
-
-def executeStages() {
-    checkout();
-    build();
-    test();
-    deploy();
-}
-
-def checkout() {
-    echo "checking out.."
-}
-
-
-def build() {
-    echo "building classess.."
-}
-
-def test() {
-    echo "testing services.."
-}
-
-def deploy() {
-    echo "deploying.."
 }
